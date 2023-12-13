e91b68d017694211d5e77d21294b106afc15302e
Surafels <ssurafel4@gmail.com>
2023-12-13 21:32:06 +0300

    Create posts, users, comments, and likes tables

---
+++ db/migrate/20231213161420_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.references :author, foreign_key: { to_table: :users}
+      t.string :title
+      t.text :text
+      t.integer :likes_counter
+
+      t.timestamps
+    end
+
+    add_index :posts, :likes_counter
+  end
+end
