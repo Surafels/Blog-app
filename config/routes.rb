ce043eb825c19d086a1589a6462864aa09d22281
Surafels <ssurafel4@gmail.com>
2023-12-18 21:09:13 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-get 'users/:user_id/posts/:id', to: 'posts#index', as: 'user_post'
+get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 get 'users', to: 'users#index', as: 'users'
 get 'users/:id', to: 'users#show', as: 'user'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
