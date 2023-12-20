b682ad1c608fb2e55dad0cdaaa21889482b7e34f
Surafels <ssurafel4@gmail.com>
2023-12-20 13:59:04 +0300

    Add required changes

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersController', type: :request do
-  let!(:user) { User.create(name: 'Tom', photo_url: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
   describe 'GET /index' do
     it 'returns a correct and successful response' do
       get users_path
@@ -21,19 +20,20 @@ RSpec.describe 'UsersController', type: :request do
   end
 
   describe 'GET/show' do
+    let!(:user) { User.create(id: 1, name: 'Tom', photo_url: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
     it 'returns a correct and successful response' do
-      get users_path(user.id)
+      get user_path(user.id)
       expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
     end
 
     it 'renders the show template' do
-      get users_path(user.id)
-      expect(response.body).to include('Here is the list of posts for the user:')
+      get user_path(user.id)
+      expect(response).to render_template(:show)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get users_path(user.id)
+      get user_path(user.id)
       expect(response.body).to include('Here is the list of posts for the user:')
     end
   end
