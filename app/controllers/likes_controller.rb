d5cb8259a43d4ff2c4d0a257fc2572ba2c8fdfc6
Surafels <ssurafel4@gmail.com>
2023-12-23 11:11:11 +0300

    Update controllers

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @user = User.find(params[:user_id])
+    @like = @post.likes.build(author: @user)
+    if @like.save
+      redirect_to user_posts_path(@user)
+    else
+      redirect_to user_posts_path(@ser), alert: 'Error creating like.'
+    end
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,23 @@ class PostsController < ApplicationController
     @user = User.find_by_id(params[:user_id])
     @post = Post.find_by_id(params[:id])
   end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find_by_id(params[:user_id])
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
