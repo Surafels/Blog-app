0432196dfda0b69117762061ddc101ca6f2c952d
Surafels <ssurafel4@gmail.com>
2024-01-16 14:31:34 +0300

    Generate devise views and update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  #get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  devise_scope :user do
+    root 'devise/sessions#new'
+    get '/user_sign_out', to: 'devise/sessions#new'
+  end
 
-  resources :users do
-  resources :posts do
-    resources :comments
-    resources :likes
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :create]
+    end
   end
-  end
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
 end
