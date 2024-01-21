51615d688ee41cf0f539193d1be9260b372a7901
Ssurafels <ssurafel4@gmail.com>
2024-01-21 10:37:55 +0300

    Update likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,10 @@
 class LikesController < ApplicationController
-  def new
-    @like = Like.new
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:post_id])
-  end
-
   def create
     @post = Post.find(params[:post_id])
-    @like = @post.likes.build(author: @user)
     @user = User.find(params[:user_id])
-
+    @like = @post.likes.build(author: @user)
     if @like.save
-      redirect_to user_posts_path(@user, @post), notice: 'Like was successfully created.'
+      redirect_to user_posts_path(@user)
     else
       redirect_to user_posts_path(@user), alert: 'Error creating like.'
     end
